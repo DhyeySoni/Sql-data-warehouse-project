4ac235d0c3c7e257b93b227c417a176a8e91b9d6
DhyeySoni <109660174+DhyeySoni@users.noreply.github.com>
2026-01-20 15:48:59 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,9 @@
+/*This script has database created with Training_DB and has three schemas init saved as 
+bronze_ds, silver_ds, gold_ds*/
+use Training_DB;
+GO
+create schema bronze_ds;
+Go
+create schema silver_ds;
+Go
+create schema gold_ds;
